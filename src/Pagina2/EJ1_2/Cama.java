d9102c27d81c48b8172117675b8ed926416569fb
Daniela <dan00.11037@gmail.com>
2026-01-28 09:44:23 +0100

    Add files via upload

---
+++ src/Pagina2/EJ1_2/Cama.java
@@ -0,0 +1,50 @@
+package Pagina2.EJ1_2;
+
+public class Cama {
+    private String estilo;
+    private int almohadas;
+    private int altura;
+    private int sabanas;
+    private int colcha;
+    private int peluche;
+
+    public Cama(int almohadas, String estilo, int altura, int sabanas, int colcha, int peluche) {
+        this.almohadas = almohadas;
+        this.estilo = estilo;
+        this.altura = altura;
+        this.sabanas = sabanas;
+        this.colcha = colcha;
+        this.peluche = peluche;
+    }
+
+    public void hacer() {
+        if (sabanas <= 0 || colcha <= 0) {
+            System.out.println("No se puede hacer la cama.");
+        } else {
+            System.out.println("Se ha hecho la cama.");
+        }
+    }
+
+    public void tenerP(){
+        if (peluche <= 0){
+            System.out.println("No tiene ningun peluche.");
+        }else {
+            System.out.println("Hay peluche(s).");
+        }
+    }
+
+    public int getAlmohadas() {
+        return almohadas;}
+
+    public String getEstilo() {
+        return estilo;}
+
+    public int getAltura() {
+        return altura;}
+
+    public int getSabanas() {
+        return sabanas;}
+
+    public int getColcha() {
+        return colcha;}
+}
---
+++ src/Pagina2/EJ1_2/Pared.java
@@ -0,0 +1,13 @@
+package Pagina2.EJ1_2;
+
+public class Pared {
+    private String direccion;
+
+    public Pared(String direccion, int posters) {
+        this.direccion = direccion;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+}
